3df71ea01d8626560192c09005ac22d444ca1dad
gustavao22 <rb227458@gmail.com>
2026-04-22 15:36:29 -0300

    Add files via upload

---
+++ atv1.py
@@ -0,0 +1,8 @@
+# Contando de 1 a 5 com while
+numero = int(input("Digite um numero positivo inteiro: "))
+
+while numero >= 0:
+    print(numero)
+    numero = numero - 1 
+
+print("Lançamento! Fim da contagem!")
---
+++ atv2.py
@@ -0,0 +1,9 @@
+nome = {"renata,joana,julio,thiago"}
+
+for i in range(4):
+    nome = input("Digite o {i+1} nome: ")
+    nomes.append(nome)
+
+print("nNomes em maiusculo:")
+for i in nomes :
+ print(nome.upper())
